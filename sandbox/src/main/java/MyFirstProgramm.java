aace7a8ee28f42c295e92655fe1bce40bffb6eeb
Виктория <cool.goodvika2077@ya.ru>
2021-04-27 11:19:44 +0300

    Добавлены методы, для вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,7 +1,27 @@
 public class MyFirstProgramm
 {
+//	public static void main (String[] args)
+//	{
+//		chislo(6);
+//	}
+//	public static void chislo(int cifra)
+//	{
+//		System.out.println("Введено число "+ cifra);
+//	}
 	public static void main (String[] args)
 	{
-		System.out.println("Privet, mir!");
+		int sm = 6;
+		double a = 2;
+		double b = 7;
+		System.out.println("Площадь квадрата со стороной "+ sm + " = " + ploshad(sm));
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = "+ ploshad(a, b));
+	}
+	public static int ploshad(int s)
+	{
+		return s*s;
+	}
+	public static double ploshad(double c, double z)
+	{
+		return c*z;
 	}
 }
